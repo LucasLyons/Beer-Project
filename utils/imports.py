3afbb8dc11a4d66f3c4ec5fa4e6924861d033d91
LucasLyons <120273381+LucasLyons@users.noreply.github.com>
2025-07-22 15:54:18 -0400

    file structure changed + data saved to folders + utils added

---
+++ utils/imports.py
@@ -0,0 +1,17 @@
+import scipy as sp
+import scipy.stats as stats
+import powerlaw as pl
+import kagglehub
+import pandas as pd
+import numpy as np
+import seaborn as sb
+import matplotlib.pyplot as plt
+import duckdb as db
+import recbole as rb
+import sklearn as sk
+import itertools
+import pickle
+from scipy.sparse import coo_matrix, csr_matrix, save_npz, load_npz
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics.pairwise import cosine_similarity
